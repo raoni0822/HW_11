6150d246e45d8a8e4d28c790822584c836be71c2
Raon Kim <raoni0822@naver.com>
2023-02-08 17:50:34 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const TOP = document.querySelector('.go_top');
+const HD = document.querySelector('#header');
+
 new Swiper('.mainVisual .swiper', {
     loop: true,
     autoplay: true,
@@ -12,9 +15,44 @@ new fullpage('#fullpage', {
     //options here
     //autoScrolling: true,
     //scrollHorizontally: true
+
+    onLeave: function (origin, destination, direction, trigger) {
+        console.log(origin.index, destination.index, direction, trigger)
+        if (direction == 'down') {
+            HD.classList.add('on');
+        }
+        else {
+            HD.classList.remove('on');
+            if (destination.index > 0) {
+                HD.classList.add('on_bg');
+            }
+            else {
+                HD.classList.remove('on_bg');
+            }
+        }
+
+        if (destination.index == 4) {
+            TOP.classList.add('on');
+            TOP.classList.remove('on02');
+        }
+        else if (destination.index == 5) {
+            TOP.classList.add('on');
+            TOP.classList.add('on02');
+        }
+        else {
+            TOP.classList.remove('on');
+            TOP.classList.remove('on02');
+        }
+
+        HD.addEventListener('click', () => {
+
+        })
+    },
 });
 
-const TIT = ['Onshore', 'Offshore', 'CS BEARING', 'CS ENERGY']
+
+
+const TIT = ['Onshore', 'Offshore', 'CS BEARING', 'CS ENERGY'];
 
 new Swiper('.mainESG .swiper', {
     loop: true,
